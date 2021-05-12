41f0bb35c28bc830010e225899674c3edf01f9fb
prongbang <prongbang@gmail.com>
2021-05-12 23:14:55 +0700

    rename custom view permission view

--- camera/src/main/java/com/prongbang/camera/widget/PermissionView.kt
+++ camera/src/main/java/com/prongbang/camera/widget/PermissionView.kt
@@ -4,15 +4,15 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
-import com.prongbang.camera.databinding.CameraPermissionViewBinding
+import com.prongbang.camera.databinding.PermissionViewBinding
 
-class CameraPermissionView @kotlin.jvm.JvmOverloads constructor(
+class PermissionView @kotlin.jvm.JvmOverloads constructor(
 		context: Context,
 		attrs: AttributeSet? = null,
 		defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
-	private var binding: CameraPermissionViewBinding = CameraPermissionViewBinding.inflate(
+	private var binding: PermissionViewBinding = PermissionViewBinding.inflate(
 			LayoutInflater.from(context))
 			.also { addView(it.root) }
 
